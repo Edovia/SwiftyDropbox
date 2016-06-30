f18cdea25b07a33d43597fabe93c6d4950f20971
Luc Vandal <luc@edovia.com>
2016-06-30 13:44:52 -0400

    Added macOS target

---
+++ SwiftyDropbox/SwiftyDropbox-macOS/SwiftyDropbox.h
@@ -0,0 +1,21 @@
+//
+//  SwiftyDropbox.h
+//  SwiftyDropbox
+//
+//  Created by Luc Vandal on 2016-06-30.
+//  Copyright © 2016 Edovia Inc. All rights reserved.
+//
+
+#ifndef SwiftyDropbox_h
+#define SwiftyDropbox_h
+
+@import Cocoa;
+#import "SecurityUtil.h"
+
+//! Project version number for SwiftyDropbox.
+FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
+
+//! Project version string for SwiftyDropbox.
+FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
+
+#endif /* SwiftyDropbox_h */
